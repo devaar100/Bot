bf5b6c243b4caa1be7aff8e7c16f16883c43be4f
devaar100 <developer.aarnav100@gmail.com>
2017-10-14 19:15:52 +0530

    finish

--- ChikuBot.py
+++ ChikuBot.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -9,4 +10,7 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn example:app --log-file=-
+web: python ChikuBot.py
